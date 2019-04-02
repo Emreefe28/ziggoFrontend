bc195cb4d18109fd5e8ac8b7acdfa18eca89194f
ryukishi <donnyschaafs@gmail.com>
2019-04-02 23:27:43 +0200

    Added create_employee component
    This is added into admin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AdminComponent } from './admin/admin.component';
+import { CreateEmployeeComponent } from './admin/create-employee/create-employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    CreateEmployeeComponent
   ],
   imports: [
     BrowserModule,
