b76c5dcf6d8b2a18543991c7c4c9538e8ad698be
ryukishi <donnyschaafs@gmail.com>
2019-04-03 22:54:53 +0200

    Added services file

--- src/app/admin/create-employee/create-employee.component.ts
+++ src/app/admin/create-employee/create-employee.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  name: string;
+  username: string;
+  password: string;
+  role: int;
   constructor() {}
 
   ngOnInit() {
+  }
+
+  sendData(): void {
+    // data: {name: this.name, username=this.username, password=this.password, role=this.role};
   }
 
 }
