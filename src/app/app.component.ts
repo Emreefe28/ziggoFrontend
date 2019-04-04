a91ea03d6ec58b641d99e26341f8daeaea34a8bc
Youssef <youby1@hva.nl>
2019-04-04 12:12:26 +0200

    test
    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'EWAZiggo-frontend';
+
+  user = {
+    name: 'Youssef',
+    age: 18
+  };
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
---
+++ src/app/translation.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TranslationService } from './translation.service';
+import { TranslateService } from '@ngx-translate/core';
+
+
+describe('TranslationService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: TranslationService = TestBed.get(TranslationService);
+    expect(service).toBeTruthy();
+  });
+});
