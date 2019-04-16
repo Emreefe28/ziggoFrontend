1eb2602fbb93b126e5a81c19595c4d378ed7b492
Youssef <youby1@hva.nl>
2019-04-16 11:04:08 +0200

    ngfor
    de ngfor loop in component gezet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EWAZiggo-frontend';
+
 }
--- src/app/questionaire/questionaire.component.ts
+++ src/app/questionaire/questionaire.component.ts
@@ -2,10 +2,48 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-questionaire',
-  templateUrl: './questionaire.component.html',
-  styleUrls: ['./questionaire.component.css']
+  styleUrls: ['./questionaire.component.css'],
+  template: `
+    DIT IS EEN TEST
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        <div class="troubleshoot-card__content mdl-card__supporting-text " align="left">
+          <div class="troubleshoot-card__layout-cell mdl-card__supporting-text">
+            <p>{{hero}}Mogelijk is er een storing in uw regio, waardoor uw internet of helemaal niet werkt of het internet een
+              stuk
+              trager is dan u gewent bent.</p>
+            <p>Controleer of er een storing in uw regio is door op deze pagina te kijken. <a
+              href="https://www.w3schools.com/"
+              target="_blank">Storing?</a></p>
+            <p>Als er geen storing in uw regio is, ga dan verder naar het volgende punt van deze checklist.</p>
+          </div>
+        </div>
+        {{ hero }}
+      </li>
+    </ul>
+
+    
+    
+  `
+
+
 })
 export class QuestionaireComponent implements OnInit {
+  title = 'Tour of Heroes';
+  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  myHero = this.heroes[0];
+
+  onClickMe() {
+    this.heroes.push("anotha one")
+  }
+
+
+  vragen = ['Vraag 1', 'Vraag 2', 'Vraag 3', 'Vraag 4'];
+  eersteVraag = this.vragen[0];
+
 
   constructor() { }
 
