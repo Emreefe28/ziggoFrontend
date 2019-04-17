3f7cdba4cea7d7214e47a6a5402df84d2eb28bde
Youssef <youby1@hva.nl>
2019-04-18 00:00:48 +0200

    Css toegevoegd en links neergezet
    Alle css is toegevoegd en questionnaire werkt nu(alhoewel de setcategory methode niet functioneert).
    
    ook is er een vieze witte bar onder questionnaire die somehow weggehaald moet worden

--- src/app/components/questionnaire/questionnaire.component.ts
+++ src/app/components/questionnaire/questionnaire.component.ts
@@ -11,16 +11,20 @@ export class QuestionnaireComponent implements OnInit {
   questions = [];
   QuestionCount=1;
   category=0;
-
+  geenVragenOver=false;
    pureQuestions=[];
 
 
   constructor(private questionnaireservice:QuestionnaireService) {
 
-
   }
 
 
+  geenVragenMeer(){
+    this.geenVragenOver=true;
+    console.log("deze methode is aangeroepen")
+
+  }
   nextQuestion(){
     // console.log("nextQuestionCount: "+this.QuestionCount
     // +"questionarray length: "+ this.questions[this.category].vragen.length);
@@ -30,8 +34,16 @@ export class QuestionnaireComponent implements OnInit {
       this.pureQuestions.push(this.questions[this.category].vragen[this.QuestionCount])
       this.QuestionCount++;
     }
+    //die if statement is niet eens echt nodig maar ach
+    else if(this.QuestionCount>=this.questions[this.category].vragen.length) {
+      console.log("er zijn geen vragen meer. Roep gerust een supercoole functie aan om chatknop " +
+        "tevoorschijn te halen");
+      this.geenVragenMeer();
+    }
 
-  }
+
+
+    }
 
   ngOnInit() {
 
