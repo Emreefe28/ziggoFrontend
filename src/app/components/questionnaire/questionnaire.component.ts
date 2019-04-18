52e18230a780cbe1ea9f36bbe40b83f411b1b504
JoseNML <33283355+JoseNML@users.noreply.github.com>
2019-04-18 08:12:51 +0200

    fixed css header

--- src/app/components/questionnaire/questionnaire.component.ts
+++ src/app/components/questionnaire/questionnaire.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {QuestionnaireService} from "../../services/questionnaire.service";
+import {Component, OnInit} from '@angular/core';
+import {QuestionnaireService} from '../../services/questionnaire.service';
 
 @Component({
   selector: 'app-questionnaire',
@@ -9,41 +9,41 @@ import {QuestionnaireService} from "../../services/questionnaire.service";
 export class QuestionnaireComponent implements OnInit {
 
   questions = [];
-  QuestionCount=1;
-  category=0;
-  geenVragenOver=false;
-   pureQuestions=[];
+  QuestionCount = 1;
+  category = 0;
+  geenVragenOver = false;
+  pureQuestions = [];
 
 
-  constructor(private questionnaireservice:QuestionnaireService) {
+  constructor(private questionnaireservice: QuestionnaireService) {
+
+  }
+
+
+  geenVragenMeer() {
+    this.geenVragenOver = true;
+    console.log('deze methode is aangeroepen');
 
   }
 
-
-  geenVragenMeer(){
-    this.geenVragenOver=true;
-    console.log("deze methode is aangeroepen")
-
-  }
-  nextQuestion(){
+  nextQuestion() {
     // console.log("nextQuestionCount: "+this.QuestionCount
     // +"questionarray length: "+ this.questions[this.category].vragen.length);
 
-    if(this.QuestionCount<this.questions[this.category].vragen.length){
+    if (this.QuestionCount < this.questions[this.category].vragen.length) {
 
-      this.pureQuestions.push(this.questions[this.category].vragen[this.QuestionCount])
+      this.pureQuestions.push(this.questions[this.category].vragen[this.QuestionCount]);
       this.QuestionCount++;
     }
-    //die if statement is niet eens echt nodig maar ach
-    else if(this.QuestionCount>=this.questions[this.category].vragen.length) {
-      console.log("er zijn geen vragen meer. Roep gerust een supercoole functie aan om chatknop " +
-        "tevoorschijn te halen");
+    // die if statement is niet eens echt nodig maar ach
+    else if (this.QuestionCount >= this.questions[this.category].vragen.length) {
+      console.log('er zijn geen vragen meer. Roep gerust een supercoole functie aan om chatknop ' +
+        'tevoorschijn te halen');
       this.geenVragenMeer();
     }
 
 
-
-    }
+  }
 
   ngOnInit() {
 
@@ -54,12 +54,17 @@ export class QuestionnaireComponent implements OnInit {
       data => {
         this.questions = data;
         this.pureQuestions.push(this.questions[this.category].vragen[0]);
-        console.log(this.category)
+        console.log(this.category);
 
       }
     );
 
   }
 
+  openChat() {
+    const params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+width=400,height=600,left=0,top=0`;
+    open('/client-chat', 'test', params);
+  }
 
 }
